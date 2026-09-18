8507f03a92d38fe859d72f99ecb60e9feb54c9a4
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-09-18 19:51:35 -0300

    V1.0.4
    aqui eu fez o esqueleto do site

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -3,5 +3,17 @@ import React, { useState, useEffect } from 'react'
 
 function Home(){
     
-    return
+    return(
+        <main>
+            <title>Presente</title>
+            <div className="salu">
+                <h2>Bem vindo ao site presente</h2>
+                <p>Por favor, leia bem antes de clicar. Aqui é so para se dirigir para o contedudo principal</p>
+            </div>
+            <div className="conteudo">
+                <h3>Clique no botão em baixo para ir no conteúdo principal</h3>
+                <button onClick={() => {}}>Clique aqui</button>
+            </div>
+        </main>
+    );
 }
---
+++ src/componentes/conteudo.jsx
@@ -0,0 +1,33 @@
+
+
+
+function Conteudo(){
+
+
+
+    return(
+        <main className="container">
+            <h1>Conteudo principal</h1>
+            <section className="mensagens">
+                <div className="txt1">
+                    <div className="animation1"></div>
+                    <div className="conteudo"></div>
+                </div>
+                <div className="txt2">
+                    <div className="animation2"></div>
+                    <div className="conteudo"></div>
+                </div>
+                <div className="txt3">
+                    <div className="animation3"></div>
+                    <div className="conteudo"></div>
+                </div>
+            </section>
+            <section className="foto">
+                <div className="foto1"></div>
+                <div className="foto2"></div>
+                <div className="foto3"></div>
+
+            </section>
+        </main>
+    );
+}
