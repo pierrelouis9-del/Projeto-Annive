0d2ddccc50c901653d285f55f8ed3c6317d8c346
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-09-18 21:27:17 -0300

    V1.0.6

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Tempo from "./componentes/tempo";
 import Conteudo from "./componentes/Conteudo";
 import Home from "./componentes/Home";
+import Acceuil from "./componentes/Acceuil";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Tempo />} />
+      <Route path="/" element={<Acceuil />} />
+      <Route path="/Tempo" element={<Tempo />} />
       <Route path="/Home" element={<Home />} />
       <Route path="/Conteudo" element={<Conteudo />} />
     </Routes>
---
+++ src/componentes/Acceuil.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Acceuil.css';
+
+
+
+function Acceuil() {
+    const navigate = useNavigate();
+
+    return(
+        <main className="acceuil">
+            <div className="text">
+                <h2>Oi, Amorcita</h2>
+                <p> <i> Preparei um presente para você!</i></p>
+                <p><i> Espero que você goste!</i></p>
+                <strong>||</strong>
+                <strong>||</strong>
+                <strong>\/</strong>
+                <button onClick={() => navigate('/Tempo')}>Começar</button>
+            </div>
+        </main>
+    );
+}
+
+export default Acceuil;
